d9d3f2166c8de8f50610e3233ee64247a1e388d7
Ramy Mohammed <rjm6947@psu.edu>
2026-03-12 18:27:15 -0400

    more changes

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ export function isPlaceholderConfig() {
   return (
     !url ||
     !anonKey ||
-    url.includes("YOUR_PROJECT_ID") ||
-    anonKey.includes("YOUR_SUPABASE_ANON_KEY")
+    url.includes("hbvjlwschtjmctrirveg") ||
+    anonKey.includes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhidmpsd3NjaHRqbWN0cmlydmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDQ4NzksImV4cCI6MjA4ODkyMDg3OX0.G6MyBZRST5MkiMqgE4sOEhg720R0NDRwWGNZGjMljoU")
   );
 }
