b5c1844d65794f1bd0223a5f63cde0c41cc8c742
Ramy Mohammed <rjm6947@psu.edu>
2026-03-12 18:57:59 -0400

    configure db

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ export function isPlaceholderConfig() {
   return (
     !hasLikelyUrl ||
     !hasLikelyAnonKey ||
-    url.includes("hbvjlwschtjmctrirveg") ||
-    anonKey.includes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhidmpsd3NjaHRqbWN0cmlydmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDQ4NzksImV4cCI6MjA4ODkyMDg3OX0.G6MyBZRST5MkiMqgE4sOEhg720R0NDRwWGNZGjMljoU")
+    url.includes("YOUR_PROJECT_ID") ||
+    anonKey.includes("YOUR_SUPABASE_ANON_KEY")
   );
 }
